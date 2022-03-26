c1005771ccdf772a83caa76d14c7b965f9243665
Matt Shull <derekshull@gmail.com>
2022-03-25 20:18:33 -0500

    real test

--- lib/longest-non-repeating-substring.rb
+++ lib/longest-non-repeating-substring.rb
@@ -1,5 +1,14 @@
 require 'byebug'
 
 def longest_non_repeating_substring(string)
-    # Your code here
+    #!!START
+    return string.size if string.size < 2
+        
+    queue, hash = [], {}
+    string.each_char.reduce(-Float::INFINITY) do |max, c|
+        hash.delete(char = queue.shift) until queue.empty? || char.eql?(c) if hash.key?(c)
+
+        [max, (queue << hash[c] = c).size].max
+    end
+    #!!END
 end
--- lib/product-of-array-except-self.rb
+++ lib/product-of-array-except-self.rb
@@ -1,5 +1,14 @@
 require 'byebug'
 
 def product_of_array_except_self(nums)
-    # Your code here
+    #!!START
+    n = nums.size
+    out = [1] * n
+    left = right = 1
+    1.upto(n-1) { |i|
+        out[i] *= left *= nums[i-1]
+        out[~i] *= right *= nums[-i]
+    }
+    out
+    #!!END
 end
